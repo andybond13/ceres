9b7408bc1d8f6da9be75849303589a44a57a20eb
Andrew Stershic <andybond13@yahoo.com>
2014-12-10 12:12:46 -0500

    added instruction to setLocation.sh

--- script/setLocation.sh
+++ script/setLocation.sh
@@ -2,6 +2,7 @@
 
 #replaces location of code in crontab.txt clears crontab, and sets crontab to crontab.txt
 crontab -r
+#edit this line!
 export ceres=~/Code/ceres
 perl -pi -e 's/\$ceres/~\/Code\/ceres/g' crontab.txt
 crontab $ceres/script/crontab.txt
